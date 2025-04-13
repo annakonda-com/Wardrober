ce480f8704b3c5b4a139c832a0fe2db172f7867b
Anna <annakondr0709a@gmail.com>
2025-04-13 11:00:44 +0300

    with apikey

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import render_template, Flask, request
 
 app = Flask(__name__)
+app.config['VK_APIKEY'] = 'YJYrGlxEqNSv2B5CjYUy'
+app.config['VK_SERVICEKEY'] = 'd7838b4cd7838b4cd7838b4c41d4acc841dd783d7838b4cb078efc3eb7a1bdd2bfce598'
 
 
 @app.route('/')
