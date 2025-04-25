44cdf0032b32094b52d3b0cde5326174385890b7
Anyalip07 <anyalip2007@gmail.com>
2025-04-25 21:55:07 +0300

    change lists in form

--- forms/add_wardrobe_item.py
+++ forms/add_wardrobe_item.py
@@ -5,12 +5,20 @@ from wtforms.fields.simple import FileField
 from wtforms.validators import DataRequired
 
 
-
 class AddWardrobeItemForm(FlaskForm):
     name = StringField('Название', validators=[DataRequired()])
     image = FileField('Загрузить фото', validators=[DataRequired()])
-    category = SelectField('Выберите категорию', choices=['верх', 'низ', '...'], coerce=str)
-    subcategory = SelectField('Выберите подкатегорию', choices=['шорты','футболки', '...'], coerce=str)
-    season = SelectField('Выберите сезон', choices=['зима', 'весна', 'лето', 'осень',], coerce=str)
-    colors = SelectField('Выберите цвет', choices=['Красный', 'blue', '...'], coerce=str)
+    category = SelectField('Выберите категорию', choices=['обувь', 'комбинезоны', 'низ',
+                                                          'платья', 'верх', 'аксессуары'], coerce=str)
+    subcategory = SelectField('Выберите подкатегорию', choices=['шорты', 'юбки', 'спортивные штаны', 'брюки', 'джинсы',
+                                                                'повседневные', 'вечерние', 'свитера', 'топы',
+                                                                'футболки', 'водолазки', 'пиджаки',
+                                                                'рубашки и блузки', 'толстовки', 'жилетки',
+                                                                'верхняя одежда', 'серьги', 'подвески', 'браслеты',
+                                                                'очки', 'головной убор', 'туфли', 'демисезонная',
+                                                                'зимняя', 'летняя'], coerce=str)
+    season = SelectField('Выберите сезон', choices=['зима', 'весна', 'лето', 'осень', ], coerce=str)
+    colors = SelectField('Выберите цвет', choices=['Белый', 'Чёрный', 'Коричневый', 'Красный', 'Розовый', 'Зелёный',
+                                                   'Синий', 'Оранжевый', 'Фиолетовый', 'Серый', 'Жёлтый', 'Бежевый',
+                                                   'Разноцветное'], coerce=str)
     submit = SubmitField('Добавить')
